0628b8723e2fd5b18c3dd3516e16582b93bab5a7
Maksuel Nascimento <mkdias_nascimento@hotmail.com>
2023-04-25 16:55:26 -0300

    feat: add new types

--- src/interfaces/movies.interfaces.ts
+++ src/interfaces/movies.interfaces.ts
@@ -2,3 +2,6 @@ import { z } from 'zod';
 import * as schemas from '../schemas';
 
 export type TMovie = z.infer<typeof schemas.movie>;
+export type TMovieRequest = z.infer<typeof schemas.movieRequest>;
+export type TMoviesList = z.infer<typeof schemas.moviesList>;
+export type TMovieUpdate = z.infer<typeof schemas.movieUpdate>;
