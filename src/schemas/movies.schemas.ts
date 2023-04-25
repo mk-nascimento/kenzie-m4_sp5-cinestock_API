0fa3e19ec521a865e09b762d2cd6cdbb3b308986
Maksuel Nascimento <mkdias_nascimento@hotmail.com>
2023-04-25 16:54:17 -0300

    feat: edit movie schema and add more schemas

--- src/schemas/movies.schemas.ts
+++ src/schemas/movies.schemas.ts
@@ -3,9 +3,11 @@ import { z } from 'zod';
 export const movie = z.object({
   id: z.number(),
   name: z.string().max(50),
-  description: z.string().optional().nullable(),
+  description: z.string().nullish(),
   duration: z.number(),
   price: z.number(),
 });
 
 export const movieRequest = movie.omit({ id: true });
+export const moviesList = movie.array();
+export const movieUpdate = movieRequest.partial();
